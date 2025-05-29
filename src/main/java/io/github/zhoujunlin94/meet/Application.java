b12ac7e607ad8320f90fc9daaa284b91121e3949
zhoujunlin <zhoujunlin@gaodun.com>
2025-05-29 17:50:42 +0800

    init

---
+++ src/main/java/io/github/zhoujunlin94/meet/Application.java
@@ -0,0 +1,17 @@
+package io.github.zhoujunlin94.meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoujunlin
+ * @date 2025-05-29-17:41
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
